2af244cb3322c02aa158e7fadfe5fb3c5fb70aad
afterlifepro <96529802+Afterlifepro@users.noreply.github.com>
2024-12-02 18:46:04 +0000

    chore: tidy code and upgrade atproto/api version

--- app/routes/oauth/callback.tsx
+++ app/routes/oauth/callback.tsx
@@ -5,11 +5,11 @@ import {
   useNavigate,
 } from "@tanstack/react-router";
 import { atclient } from "@/server/client-metadata";
+import { encryptString } from "@/lib/encryption";
+import { env } from "@config";
 import { createServerFn } from "@tanstack/start";
 import { setCookie } from "vinxi/http";
 import { useState } from "react";
-import { env } from "@config";
-import { encryptString } from "@/lib/encryption";
 import { decodeBase64, encodeBase64urlNoPadding } from "@oslojs/encoding";
 
 export const Route = createFileRoute("/oauth/callback")({
@@ -18,7 +18,6 @@ export const Route = createFileRoute("/oauth/callback")({
     await getAuthSession(location.searchStr.slice(1)),
 });
 
-// TODO change to use the location url and extract params server side
 const getAuthSession = createServerFn("GET", async (paramsStr: string) => {
   const params = new URLSearchParams(paramsStr);
   const { session } = await atclient.callback(params);
@@ -46,21 +45,3 @@ function RouteComponent() {
     ""
   );
 }
-
-/*
-export async function GET({ request, cookies }: RequestEvent) {
-  const params = new URLSearchParams(request.url.split("?")[1]);
-  try {
-    const { session } = await atclient.callback(params)
-    const key = decodeBase64(MYB_PASSWORD);
-    const encrypted = await encryptString(key, session.did);
-    const encoded = encodeBase64urlNoPadding(encrypted);
-    cookies.set("sid", encoded, { path: "/", maxAge: 60 * 60, httpOnly: true, sameSite: "lax" });
-  } catch (err) {
-    console.log(err);
-    error(500, { message: (err as Error).message });
-  }
-
-  redirect(301, "/");
-}
-*/
